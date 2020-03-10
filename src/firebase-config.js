4964d0b04dc12a5721d5ab9839072c6660744d4d
Belen Montalvo <bmontalvo@Belens-MBP.home>
2020-03-10 23:44:17 +0100

    init version

---
+++ src/firebase-config.js
@@ -0,0 +1,32 @@
+import firebase from 'firebase/app';
+import 'firebase/messaging';
+
+export const Firebase = {
+  init() {
+    const config = {
+      apiKey: "AIzaSyDBRBj0L5qUGmtSopVim_VUsfFIuWns4Hk",
+      authDomain: "vuevixens-pwa.firebaseapp.com",
+      databaseURL: "https://vuevixens-pwa.firebaseio.com",
+      projectId: "vuevixens-pwa",
+      storageBucket: "vuevixens-pwa.appspot.com",
+      messagingSenderId: "1056270913057",
+      appId: "1:1056270913057:web:91d0626bf1607917d55d60",
+      measurementId: "G-XG602G5TGZ"
+    };
+    
+    firebase.initializeApp(config);
+    console.log('initialized firebase')
+  },
+
+  messaging() {
+    const msg = firebase.messaging();
+  
+    msg.usePublicVapidKey("BFVCdJCcqctSUqTRaIW11kvUKb3iTCwBVTYoc7AHdIlKMyBZvPE_VZV2dLKxk0PyjGGveM9r59gGy-SGienLboI");
+
+    console.log('configurado firebase messaging')
+
+    return msg;
+  }
+}
+
+export default Firebase
